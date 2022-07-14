83cdd2d547fcc6636bf0c0c037606dbd7ae121b2
gilangcsy <gilangchandra9@gmail.com>
2022-07-14 18:00:22 +0700

    add database config for production

---
+++ app/config/dev-prod.js
@@ -0,0 +1,15 @@
+module.exports = {
+    HOST: 'ec2-23-23-182-238.compute-1.amazonaws.com',
+    USER: 'teeelcsxkgswib',
+    PASSWORD: 'a9dbdbfebcb5be72bbd43e28b20eac1e667541b3a17b1f830ac0994a13d98b54',
+    DB: 'dagki726q806bf',
+    DIALECT: 'postgres',
+    POOL: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    },
+    TIMEZONE: "+07:00",
+    VERSION: '/api/v1'
+}
