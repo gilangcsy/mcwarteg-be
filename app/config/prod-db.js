b8c55d2f27049dc6800cc1dbc7b1ecc11a5d8777
gilangcsy <gilangchandra9@gmail.com>
2022-07-14 19:25:00 +0700

    mengubah config db

--- app/config/prod-db.js
+++ app/config/prod-db.js
@@ -12,5 +12,5 @@ module.exports = {
     },
     TIMEZONE: "+07:00",
     VERSION: '/api/v1',
-    URL: 'postgres://teeelcsxkgswib:a9dbdbfebcb5be72bbd43e28b20eac1e667541b3a17b1f830ac0994a13d98b54@ec2-23-23-182-238.compute-1.amazonaws.com:5432/dagki726q806bf'
+    URL: 'postgres://wfosbltdxzufgb:78fee863683ede8b2be549a24813dd3c9ebde37e65454e0454e88192519dce57@ec2-3-223-169-166.compute-1.amazonaws.com:5432/d9utt417imie27'
 }
